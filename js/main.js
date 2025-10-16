a29ea590d954250de2760e358fc4adaec982ec1f
Caio <caiob7517@gmail.com>
2025-10-16 14:03:00 -0300

    feat/Lògica de càlculo implementada

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+let Teste = document.getElementById("teste")
 let totalSemana = document.getElementById("totalSemana")
 let totalMedia = document.getElementById("totalMedia")
 let inputSegunda = document.getElementById("input-segunda")
@@ -10,13 +11,34 @@ let inputDomingo = document.getElementById("input-domingo")
 
 
 function CalcularTotalMedia() {
-    let total = Number(inputSegunda.value) + Number(inputTerca.value) + Number(inputQuarta.value) + Number(inputQuinta.value) + Number(inputSexta.value) + Number(inputSabado.value) + Number(inputDomingo.value)
+
+  let total = Number(inputSegunda.value) +
+              Number(inputTerca.value) +
+              Number(inputQuarta.value) +
+              Number(inputQuinta.value) +
+              Number(inputSexta.value) +
+              Number(inputSabado.value) +
+              Number(inputDomingo.value);
 
     let media = total / 7
 
     totalSemana.innerText = "💰 Você gastou ao todo R$ " + total.toFixed(2) + " nesta semana!"
 
     totalMedia.innerText = "📊 Sua média diária foi de R$ " + media.toFixed(2)
+
+}
+
+
+function teste() {
+
+    let totalGasto = 0
+
+    for (let i = 1; i < 8; i++) {
+        let gasto = Teste
+        gasto = parseFloat(gasto.value)
+        totalGasto += gasto
+        console.log(totalGasto)
+    }
 
 }
 
